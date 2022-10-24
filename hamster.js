9890d577bb1c4ab604a98a1f93d0575b162f5376
yingwerica <yingw.pro@gmail.com>
2022-10-24 15:14:26 -0600

    created Person class

--- hamster.js
+++ hamster.js
@@ -1,3 +1,4 @@
+//creat Hamster class
 class Hamster {
     constructor(name){
       this.owner = ''
@@ -14,3 +15,50 @@ class Hamster {
       return this.price
     }
   }
+
+  let aHamster = new Hamster;
+  console.log(aHamster.getPrice());
+
+ //creat Person class
+  class Person {
+    constructor(name){
+      this.name = name
+      this.age = 0
+      this.height = 0
+      this.weight = 0
+      this.mood = 0
+      this.hamsters = []
+      this.bankAccount = 0
+    }
+    getName(){
+      return this.name
+    }
+    getAge(){
+      return this.age
+    }
+    getWeight(){
+      return this.weight
+    }
+    greet(){
+      console.log(`I am ${this.name} hello`)
+    }
+    eat(){
+      this.weight++
+      this.mood++
+    }
+    exercise(){
+      this.weight--
+    }
+    ageUp(){
+      this.age++
+      this.height++
+      this.weight++
+      this.mood--
+      this.bankAccount+=10
+    }
+    buyHamster(hamster){
+      this.hamsters.push(hamster)
+      this.mood+=10
+      this.bankAccount-=hamster.getPrice()
+    }
+}
