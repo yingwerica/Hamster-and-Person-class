1a058a7dc59e19e7791e2c076db489f215fed359
yingwerica <yingw.pro@gmail.com>
2022-10-24 16:33:20 -0600

    created timmy's story

--- hamster.js
+++ hamster.js
@@ -62,3 +62,45 @@ class Hamster {
       this.bankAccount-=hamster.getPrice()
     }
 }
+
+
+// Instantiate a new Person named Timmy
+let timmy = new Person("Timmy");
+// Age Timmy five years
+for (let i = 0; i < 5; i++) {
+    timmy.ageUp();
+}
+// At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+for (let i = 0; i < 5; i++) {
+    timmy.eat();
+}
+// Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+for (let i = 0; i < 5; i++) {
+    timmy.exercise();
+}
+// Age Timmy 9 years
+for (let i = 0; i < 9; i++) {
+    timmy.ageUp();
+}
+// Create a hamster named "Gus"
+let gus = new Hamster("Gus");
+// Set Gus's owner to the string "Timmy"
+gus.owner = "Timmy";
+// Have Timmy "buy" Gus
+timmy.buyHamster(gus);
+// Age Timmy 15 years
+for (let i = 0; i < 15; i++) {
+    timmy.ageUp();
+}
+// Have Timmy eat twice
+// for (let i = 0; i < 2; i++) {
+//     timmy.eat();
+// }
+timmy.eat()
+timmy.eat()
+// Have Timmy exercise twice
+// for (let i = 0; i < 2; i++) {
+//     timmy.exercise();
+// }
+timmy.exercise()
+timmy.exercise()
